e4fbd90b51e0302065db937705a4aded412d3be6
Timeox2k <schockilp@gmail.com>
2021-12-30 01:13:20 +0100

    Added Notification

--- src/main/java/org/lara/byelog4shell/byelog4shell/listener/PlayerChatListener.java
+++ src/main/java/org/lara/byelog4shell/byelog4shell/listener/PlayerChatListener.java
@@ -2,6 +2,7 @@ package org.lara.byelog4shell.byelog4shell.listener;
 
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.ChatEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.api.plugin.Plugin;
@@ -15,11 +16,18 @@ public class PlayerChatListener implements Listener {
 
     @EventHandler
     public void onChat(ChatEvent event) {
-        String message = event.getMessage();
+        final String message = event.getMessage();
+        final ProxiedPlayer proxiedPlayer = (ProxiedPlayer) event.getSender();
+        final ProxyServer proxyServer = ProxyServer.getInstance();
 
         if(message.toLowerCase().startsWith("${jndi:")) {
             event.setCancelled(true);
-            event.getSender().disconnect(new TextComponent("§cHow stupid do you think we are?\n\n§c§lGo get a Job."));
+            proxiedPlayer.disconnect(new TextComponent("§cHow stupid do you think we are?\n\n§c§lGo get a Job."));
+
+            final String prefix = "§cByeLog4Shell §8> ";
+            proxyServer.getPlayers().forEach(player -> {if (player.hasPermission("ByeLog4Shell.notify")) proxiedPlayer.sendMessage(new TextComponent(prefix + "§cThe Player §4" + player.getName() + " §ctried to use §4Log4Shell§c!"));});
+            proxyServer.getLogger().info(proxiedPlayer.getName() + " tried to use the Log4Shell Exploit!");
+
         }
     }
 
